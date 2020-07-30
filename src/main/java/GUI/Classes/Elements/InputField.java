d7f9b2140a7e0715ee2e322bb647abcb3e189ceb
unknown <jominator2@gmail.com>
2020-07-30 21:43:04 +0300

    first commit

---
+++ src/main/java/GUI/Classes/Elements/InputField.java
@@ -0,0 +1,2 @@
+package GUI.Classes.Elements;public class InputField {
+}
---
+++ src/main/java/Logic/Main.java
@@ -0,0 +1,7 @@
+import GUI.Classes.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame mainFrame = MainFrame.getInstance();
+    }
+}
