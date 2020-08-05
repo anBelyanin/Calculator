14b4ddd2a2253ed16fae3fe369fa821625023cd4
unknown <jominator2@gmail.com>
2020-08-05 21:51:11 +0300

    add InputField to commit pull

--- src/main/java/GUI/Classes/Elements/InputField.java
+++ src/main/java/GUI/Classes/Elements/InputField.java
@@ -1,2 +1,27 @@
-package GUI.Classes.Elements;public class InputField {
+package GUI.Classes.Elements;
+
+import GUI.Interfaces.GuiElement;
+
+import javax.swing.*;
+
+public class InputField extends JTextField implements GuiElement {
+    private static JTextField inputField;
+    private static InputField instance;
+
+    private InputField() {
+        this.run();
+    }
+
+    public static InputField getInstance() {
+
+        if (instance == null) instance = new InputField();
+
+        return instance;
+    }
+
+    @Override
+    public void run() {
+        inputField = new JTextField(30);
+        inputField.setVisible(true);
+    }
 }
