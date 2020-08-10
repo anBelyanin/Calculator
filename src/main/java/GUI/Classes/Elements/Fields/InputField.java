2b5b2728734a4be1daa2292f4c2badc9789bd252
unknown <jominator2@gmail.com>
2020-08-11 01:01:53 +0300

    total refactoring

--- src/main/java/GUI/Classes/Elements/Fields/InputField.java
+++ src/main/java/GUI/Classes/Elements/Fields/InputField.java
@@ -1,2 +1,24 @@
-package GUI.Classes.Elements.Fields;public class InputField {
+package GUI.Classes.Elements.Fields;
+
+import GUI.Interfaces.GuiElement;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class InputField extends JTextField implements GuiElement {
+
+    private static InputField inputField;
+
+    private InputField() {
+        this.setColumns(20);
+        this.setLayout(new FlowLayout(FlowLayout.CENTER));
+        this.setVisible(true);
+    }
+
+    public static InputField getInputField() {
+
+        if (inputField == null) inputField = new InputField();
+
+        return inputField;
+    }
 }
